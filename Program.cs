0c69872f3e84c3f20145c63fe404017cdf4c6ca4
M.Haris Khan <hkhan@cedrus.com>
2023-07-18 14:26:42 +0500

    Feat;ThirdDistinct

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 3, 2, 1 };
+        int result = FindThirdMax(nums);
+        Console.WriteLine(result);
+        Console.ReadLine();
+       
+    }
+
+    public static int FindThirdMax(int[] nums)
+    {
+        Array.Sort(nums);  
+        Array.Reverse(nums);  
+
+        int distinctMaxCount = 0;
+        int? thirdMax = null;
+
+        foreach (int num in nums)
+        {
+            if (num != thirdMax)
+            {
+                thirdMax = num;
+                distinctMaxCount++;
+
+                if (distinctMaxCount == 3)
+                    return thirdMax.Value;
+            }
+           
+        }
+
+        return nums[0];
+        
+    }
+}
